3e930709898fdaafadcfdced9d9a091a53a4f25b
Zoltán Lehoczky <zlehoczky@diligent.com>
2024-10-23 17:43:27 +0200

    add basic auth

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,39 @@
 <script setup lang="ts">
+import { computed, ref, watch } from "vue";
 import * as netlifyIdentity from "netlify-identity-widget";
 
-netlifyIdentity.on("login", (user) => {
-  console.log({ user });
+netlifyIdentity.init({
+  locale: "en",
+});
+
+const user = ref<netlifyIdentity.User | null>(netlifyIdentity.currentUser());
+
+netlifyIdentity.on("init", (_user) => {
+  user.value = _user;
+});
+
+netlifyIdentity.on("login", (_user) => {
+  user.value = _user;
+  netlifyIdentity.close();
+});
+
+netlifyIdentity.on("logout", () => {
+  user.value = null;
 });
 </script>
 
 <template>
-  <main>hello</main>
-  <button @click="netlifyIdentity.open()">Open</button>
+  <main>
+    <template v-if="user">
+      <main>hello, {{ user.email }}</main>
+
+      <button @click="netlifyIdentity.logout()">Logout</button>
+    </template>
+
+    <template v-else>
+      <button @click="netlifyIdentity.open('login')">Login</button>
+    </template>
+  </main>
 </template>
 
 <style scoped></style>
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,5 @@
 import { createApp } from "vue";
 import "./style.css";
 import App from "./App.vue";
-import * as netlifyIdentity from "netlify-identity-widget";
-
-netlifyIdentity.init({
-  APIUrl: "",
-  locale: "en",
-});
 
 createApp(App).mount("#app");
